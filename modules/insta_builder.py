2d67fcaea84cee3640bf494e3dbbc227c5abfb67
Skyler Burger <skylerburger@gmail.com>
2020-07-03 18:35:43 -0700

    Updated notebook

--- modules/insta_builder.py
+++ modules/insta_builder.py
@@ -3,7 +3,6 @@ import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 
